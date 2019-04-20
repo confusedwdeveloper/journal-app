e1f0261bd5a287e04e487b2a930ae0f34cb21c5a
Darshan Datta <darshandatta.10@gmail.com>
2019-04-20 13:40:45 +0530

    todo module created and tested

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './scss/styles.scss'
 import { getJournals, loadJournals, createJournal, removeJournal, sortJournals, updateJournalText } from './journals'
 import { getFilters, setFilters } from './filters'
+import { getTodos, createTodo, removeTodo, toggleTodo } from './todos'
+
 
---
+++ src/todos.js
@@ -0,0 +1,58 @@
+
+import { getJournals } from './journals'
+import uuidv4 from 'uuid/v4'
+
+// function to expose todos
+const getTodos = (journalId) => {
+    const journal = getJournals().find((entry) => entry.id === journalId)
+    if (!journal) {
+        return []
+    }
+    try {
+        return journal.todos
+    } catch (e) {
+
+    }
+}
+
+// function to create todo
+const createTodo = (journalId, text) => {
+    const journals = getJournals()
+    const journal = journals.find((entry) => entry.id === journalId)
+    const id = uuidv4()
+
+    journal.todos.push({
+        id,
+        text,
+        completed: false
+    })
+    localStorage.setItem('journal', JSON.stringify(journals))
+}
+
+// A function to remove a todo based on id
+const removeTodo = (journalId, todoId) => {
+    const journals = getJournals()
+    const journal = journals.find((entry) => entry.id === journalId)
+    // index of todo to remove
+    const todoIndex = journal.todos.findIndex((todo) => todo.id === todoId )
+
+    if (todoIndex > -1) {
+        journal.todos.splice((todoIndex, 1))
+        localStorage.setItem('journal', JSON.stringify(journals))
+    }
+}
+
+// A function to toggle todo completed status
+
+const toggleTodo = (journalId, todoId) => {
+const journals = getJournals()
+const journal = journals.find((entry) => entry.id === journalId)
+const todo = journal.todos.find((todo) => todo.id === todoId)
+if (todo) {
+    todo.completed = !todo.completed
+    localStorage.setItem('journal', JSON.stringify(journals))
+}
+}
+
+// setting up exports
+export { getTodos, createTodo, removeTodo, toggleTodo }
