8cdd47fff179a381bf11c86a9d75ada396d3b108
Darshan Datta <darshandatta.10@gmail.com>
2019-04-20 11:16:56 +0530

    filters module created and tested

---
+++ src/filters.js
@@ -0,0 +1,28 @@
+const filters = {
+    todoSearchText: '',
+    journalsSearchText: '',
+    hideComleted: false, // toggle todo
+    sortBy: 'byEdited' // sort journal entries
+}
+
+// expose filters to other files
+const getFilters = () => filters
+
+// function to set/change filters
+const setFilters = ({ todoSearchText, journalsSearchText, hideComleted, sortBy }) => {
+    if (typeof todoSearchText === 'string') {
+        filters.todoSearchText = todoSearchText
+    }
+    if ( typeof journalsSearchText === 'string') {
+        filters.journalsSearchText = journalsSearchText
+    }
+    if (typeof hideComleted === 'boolean') {
+        filters.hideComleted = hideComleted
+    }
+    if (typeof sortBy === 'string') {
+        filters.sortBy = sortBy
+    }
+}
+
+// setting up exports
+export { getFilters, setFilters }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './scss/styles.scss'
 import { getJournals, loadJournals, createJournal, removeJournal, sortJournals, updateJournalText } from './journals'
+import { getFilters, setFilters } from './filters'
 
